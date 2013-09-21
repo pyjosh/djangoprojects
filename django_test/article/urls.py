0225173efe5fcb0de78239f26a5eca9c4d7d7a6e
pyjosh <richard.richdude@gmail.com>
2013-09-21 16:17:34 +0200

    add url to match language session view

--- django_test/article/urls.py
+++ django_test/article/urls.py
@@ -3,4 +3,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^all/$', 'article.views.articles'),
     url(r'^get/(?P<article_id>\d+)/$', 'article.views.article'),
+    # for session language
+    url(r'^language/(?P<language>[a-z\-]+)/$', 'article.views.language'),
+
 )
