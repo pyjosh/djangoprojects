ab335f709162ae067fbcaf27d4654ab2382692a0
pyjosh <richard.richdude@gmail.com>
2013-09-21 16:15:31 +0200

    add session handling to views

--- django_test/article/views.py
+++ django_test/article/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render_to_response
 from article.models import Article
+# for sessions and cookies
+from django.http import HttpResponse
+
 
 def articles(request):
-    return render_to_response('articles.html', {'articles': Article.objects.all()})
+    # this on is stored in cookies
+    language = 'en-gb'
+    # this one is stored in session
+    session_language = 'en-gb'
+
+    if 'lang' in request.COOKIES:
+        language = request.COOKIES['lang']
+
+    return render_to_response('articles.html',
+        {'articles': Article.objects.all(),
+        'language' : language}
+        )
 
 def article(request, article_id):
     return render_to_response('article.html', {'article': Article.objects.get(id=article_id)})
+
+
+
+def language(request, language='en-gb'):
+    # this is rendered to the browser window - will print this on the page
+    respone = HttpResponse("seting language to %s " % language)
+
+    respone.set_cookie('lang', language)
+
+    return respone
