7af02c823e0d732e7424593f1e664359280323ca
pyjosh <richard.richdude@gmail.com>
2013-09-21 11:52:23 +0200

    add entry in urls

--- django_test/article/views.py
+++ django_test/article/views.py
@@ -12,5 +12,5 @@ def hello(request):
 def hello_template(request):
     name = "Mike"
     t = get_template('hello.html')
-    html = t.render(Context({'name' : name}))
+    html = t.render(Context({'name': name}))
     return HttpResponse(html)
