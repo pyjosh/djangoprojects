1998b89806a1651404e0b4fc92b1eb37ee16cb4c
pyjosh <richard.richdude@gmail.com>
2013-09-21 11:50:16 +0200

    add view hello_template

--- django_test/article/views.py
+++ django_test/article/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
-
+# helper function that uses settings.py to find templates and get them
+from django.template.loader import get_template
+# templates have a Context object to insert data we have generated to the template
+from django.template import Context
 
 def hello(request):
     name = "Mike"
     html = "<html><body>Hi %s welcome! </body></html>" % name
+    return HttpResponse(html)
+
+def hello_template(request):
+    name = "Mike"
+    t = get_template('hello.html')
+    html = t.render(Context({'name' : name}))
     return HttpResponse(html)
