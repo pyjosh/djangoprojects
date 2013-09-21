d82b0a2127670dcf243ce4b280a7101fd76bbecd
pyjosh <richard.richdude@gmail.com>
2013-09-21 11:08:10 +0200

    add model article

--- django_test/article/models.py
+++ django_test/article/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateTime('date published')
+    likes = model.IntegerField()
