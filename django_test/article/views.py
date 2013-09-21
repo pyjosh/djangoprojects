b5c80fb2e69f2ee1d898e212fbc6e425571776e4
pyjosh <richard.richdude@gmail.com>
2013-09-21 11:35:17 +0200

    add hello view

--- django_test/article/views.py
+++ django_test/article/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.http import HttpResponse
+
+
+def hello(request):
+    name = "Mike"
+    html = "<html><body>Hi %s welcome! </body></html>" % name
+    return HttpResponse(html)
